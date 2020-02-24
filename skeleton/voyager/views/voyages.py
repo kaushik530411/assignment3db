053b4549992aa76bcef6b4728a2ff02fee37f87a
Kaushik Mishra <kaushik530411@gmail.com>
2020-02-23 20:53:23 -0800

    Implemented the Voyages views.

--- skeleton/voyager/views/voyages.py
+++ skeleton/voyager/views/voyages.py
@@ -5,7 +5,12 @@ from flask import request
 
 from voyager.db import get_db, execute
 
+def get_all_voyages(conn):
+    return execute(conn, "SELECT v.sid, v.bid, v.date_of_voyage FROM Voyages AS v")
+
 def views(bp):
     @bp.route("/voyages")
-    def _voyages():
-        return 'not implemented'
+    def _get_all_voyages():
+        with get_db() as conn:
+            rows = get_all_voyages(conn)
+        return render_template("table.html", name="voyages", rows=rows)
