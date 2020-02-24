874444a162d8ae93001549ec74ad2f09c31c8f65
Kaushik Mishra <kaushik530411@gmail.com>
2020-02-23 23:17:11 -0800

    Implemented SQL support for List the boats by order of popularity (number of voyages) from most popular to least.

--- skeleton/voyager/views/boats.py
+++ skeleton/voyager/views/boats.py
@@ -14,6 +14,10 @@ def get_boats_from_sailor_name(conn, sailor_name):
     return execute(conn, 
     "SELECT DISTINCT Boats.bid, Boats.name  FROM ((Sailors INNER JOIN Voyages ON Voyages.sid = Sailors.sid) INNER JOIN Boats ON Voyages.bid = Boats.bid) WHERE Sailors.name = :s_name", {'s_name': sailor_name})
 
+def get_boats_by_popularity(conn):
+    return execute(conn, 
+    "SELECT DISTINCT Boats.bid, Boats.name, COUNT(Boats.bid) as num_of_voyages  FROM (Boats INNER JOIN Voyages ON Voyages.bid = Boats.bid) GROUP BY Boats.bid ORDER BY COUNT(Boats.bid) DESC")
+
 def views(bp):
     @bp.route("/boats")
     def _boats():
@@ -27,3 +31,9 @@ def views(bp):
             sailor_name = request.args.get('sailor-name')
             rows = get_boats_from_sailor_name(conn, sailor_name)
         return render_template("table.html", name="Boats sailed by sailor: " + sailor_name, rows=rows)
+    
+    @bp.route("/boats/by-popularity")
+    def _get_boats_by_popularity():
+        with get_db() as conn:
+            rows = get_boats_by_popularity(conn)
+        return render_template("table.html", name="Boats ordered by popularity (Num of Voyages): ", rows=rows)
